df50f83fb183abda785844412a24da92cd72cf8f
Ismael Junior <ismaeljcjunior@gmail.com>
2023-02-03 17:54:22 -0300

    Update server2.js

--- src/server2.js
+++ src/server2.js
@@ -31,41 +31,45 @@ app.post("/convert-to-excel", (req, res) => {
 });
 
 app.post("/download", async (req, res) => {
-  
-  const testJsonToXlsx = async () => {
-    const workbook = new excel4node.Workbook();
-    const worksheet = workbook.addWorksheet("Relátorio");
+  const data = req.body
 
-    const resultSQL = await dbMssql.query(process.env.SELECT_BASE, {
-      type: dbMssql.QueryTypes.SELECT,
-    });
-    const headers = Object.keys(resultSQL[0]);
-    headers.forEach((header, index) => {
-      worksheet.cell(1, index + 1).string(header);
-    });
-    resultSQL.forEach((data, dataIndex) => {
-      headers.forEach((header, headerIndex) => {
-        const value = data[header];
-        if (typeof value === "number" || typeof value === "object") {
-          worksheet
-            .cell(dataIndex + 2, headerIndex + 1)
-            .string(JSON.stringify(value));
-        } else {
-          worksheet.cell(dataIndex + 2, headerIndex + 1).string(value);
-        }
-      });
-    });
-    res.setHeader(
-      "Content-Type",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    res.setHeader("Content-Disposition", "attachment; filename=data.xlsx");
+  res.json(data);
 
-    workbook.write("data.xlsx", res);
 
-    console.log(resultSQL);
-  };
-  testJsonToXlsx();
+  // const testJsonToXlsx = async () => {
+  //   const workbook = new excel4node.Workbook();
+  //   const worksheet = workbook.addWorksheet("Relátorio");
+
+  //   const resultSQL = await dbMssql.query(process.env.SELECT_BASE, {
+  //     type: dbMssql.QueryTypes.SELECT,
+  //   });
+  //   const headers = Object.keys(resultSQL[0]);
+  //   headers.forEach((header, index) => {
+  //     worksheet.cell(1, index + 1).string(header);
+  //   });
+  //   resultSQL.forEach((data, dataIndex) => {
+  //     headers.forEach((header, headerIndex) => {
+  //       const value = data[header];
+  //       if (typeof value === "number" || typeof value === "object") {
+  //         worksheet
+  //           .cell(dataIndex + 2, headerIndex + 1)
+  //           .string(JSON.stringify(value));
+  //       } else {
+  //         worksheet.cell(dataIndex + 2, headerIndex + 1).string(value);
+  //       }
+  //     });
+  //   });
+  //   res.setHeader(
+  //     "Content-Type",
+  //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  //   );
+  //   res.setHeader("Content-Disposition", "attachment; filename=data.xlsx");
+
+  //   workbook.write("data.xlsx", res);
+
+  //   console.log(resultSQL);
+  // };
+  // testJsonToXlsx();
 });
 
 app.get("/", (req, res) => {
